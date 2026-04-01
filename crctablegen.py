23db6ecc00f812bfa64ab90851a35b698b7d0d26
vahgon <mullins.ian@outlook.com>
2026-04-01 16:43:38 -0400

    Updated help messages to more so conform in diction

--- crctablegen.py
+++ crctablegen.py
@@ -30,42 +30,42 @@ def main() -> tuple[Namespace, Callable[[], None]]:
                       dest="poly", help="the hexadecimal representation of chosen polynomial")
 
     poly.add_argument("-d", "--degree", type=int, metavar="INT", dest="degree", default=32,
-                      choices=(8, 16, 32), help="polynomial degree. default = 32")
+                      choices=(8, 16, 32), help="degree of polynomial (default = 32)")
 
     poly.add_argument("-r", "--reflect-poly", dest="reflected", action="store_true",
-                      help="use the LSB implementation when computing table")
+                      help="perform right-shift (LSB) operations on polynomial while computing table")
 
     #  Formatting args
-    form.add_argument("-c", "--container", metavar="CHAR", dest="container", choices=['b', 'c'],
-                      help="surround entire table in CHAR. choose from ['b'racket, 'c'urly].")
+    form.add_argument("-c", "--container", metavar="STR", dest="container", choices=['b', 'c'],
+                      help="surround entire table in STR - choose from ['b'racket, 'c'urly].")
 
-    form.add_argument("-s", "--separator", type=str, metavar="CHAR(S)", dest="sep", default=" ",
-                      help="will separate every byte in hex table elements with CHAR(S).")
+    form.add_argument("-s", "--separator", type=str, metavar="STR", dest="sep", default=" ",
+                      help="will separate elements with STR (default = \" \")")
 
     form.add_argument("-i", "--indent", type=str, metavar="INT", dest="indent",
-                      help="specify indentation amount. (4 = tab char, default = 0)")
+                      help="specify indent width (default = 0)")
 
     form.add_argument("--prefix", dest="prefix", action='store_true',
                       help="append `0x` to all table elements")
 
+    col_row_opts.add_argument("--row-len", type=int, metavar="INT", dest='rlen',
+                              help="specify the amount of table elements to append to a given line")
+
     col_row_opts.add_argument("--horizontal", action="store_true", dest="hori",
-                              help="output all values on the same line")
+                              help="output all table elements on the same line")
 
     col_row_opts.add_argument("--vertical", action="store_true", dest="vert",
-                              help="output all calculated values on a newline")
-
-    col_row_opts.add_argument("--row-len", type=int, metavar="INT", dest='rlen',
-                              help="specify # of elements to print before appending a newline")
+                              help="output each table element on a newline")
 
     #  Core args
     core.add_argument("-o", "--output", type=Path, metavar="FILE", dest="output",
-                      default="./output", help="will write or append table contents to to FILE.")
+                      help="write lookup table to FILE")
 
     core.add_argument("-h", "--help", dest="help", action="store_true", help="display help")
 
     core.add_argument("-v", "--verbose", dest="verbose", action="store_const",
                       default=logging.WARN, const=logging.INFO,
-                      help="suppress all output except for finished table.")
+                      help="print updates from the logger as script executes")
 
     return (parser.parse_args(), parser.print_help)
 
